044ea5b7f0bf1c259450ed757e09cd4649947a00
Amir Ajorloo <Amir@Amirs-MacBook-Pro.local>
2014-07-16 15:47:48 +0430

    plates and in-page effect added

--- static/js/home.js
+++ static/js/home.js
@@ -9,5 +9,53 @@ function imReady()
 	$('#main-text-plate').css({'margin-top':heightOffset.toString()+'px','margin-bottom':heightOffset.toString()+'px'})
 	//$('#main-pic').css({'background-size':($(window).height()*0.4).toString()+'px'})
 	
+	$(window).scroll(function()
+	{
+		console.log($(document).scrollTop())
+		if($(document).scrollTop()<$('#category-plate').position().top-200)
+		{
 
+		}
+		else if($(document).scrollTop()>$('#category-plate').position().top-200 && $('#category-plate').hasClass('in-side'))
+		{
+			console.log("category")
+			$('#category-image').css({'animation':'from-left 0.7s','-webkit-animation':'from-left 0.7s','animation-fill-mode': 'forwards'});
+			$('#category-text').css({'animation':'from-right 0.7s','-webkit-animation':'from-right 0.7s','animation-fill-mode': 'forwards'});
+			$('#category-plate').removeClass('in-side');
+		}
+		else if($(document).scrollTop()>$('#subject-plate').position().top-200 && $('#subject-plate').hasClass('in-side'))
+		{
+			console.log("subject")
+			$('#subject-image').css({'animation':'from-left 0.7s','-webkit-animation':'from-left 0.7s','animation-fill-mode': 'forwards'});
+			$('#subject-text').css({'animation':'from-right 0.7s','-webkit-animation':'from-right 0.7s','animation-fill-mode': 'forwards'});
+			$('#subject-plate').removeClass('in-side');
+		}
+		else if($(document).scrollTop()>$('#ai-plate').position().top-200 && $('#ai-plate').hasClass('in-side'))
+		{
+			console.log("ai")
+			$('#ai-image').css({'animation':'from-left 0.7s','-webkit-animation':'from-left 0.7s','animation-fill-mode': 'forwards'});
+			$('#ai-text').css({'animation':'from-right 0.7s','-webkit-animation':'from-right 0.7s','animation-fill-mode': 'forwards'});
+			$('#ai-plate').removeClass('in-side');
+		}
+		else if($(document).scrollTop()>$('#lost-plate').position().top-200 && $('#lost-plate').hasClass('in-side'))
+		{
+			console.log("lost")
+			$('#lost-image').css({'animation':'from-left 0.7s','-webkit-animation':'from-left 0.7s','animation-fill-mode': 'forwards'});
+			$('#lost-text').css({'animation':'from-right 0.7s','-webkit-animation':'from-right 0.7s','animation-fill-mode': 'forwards'});
+			$('#lost-plate').removeClass('in-side');
+		}
+		else if($(document).scrollTop()>$('#source-plate').position().top-200 && $('#source-plate').hasClass('in-side'))
+		{
+			console.log("source")
+			$('#source-image').css({'animation':'from-left 0.7s','-webkit-animation':'from-left 0.7s','animation-fill-mode': 'forwards'});
+			$('#source-text').css({'animation':'from-right 0.7s','-webkit-animation':'from-right 0.7s','animation-fill-mode': 'forwards'});
+			$('#source-plate').removeClass('in-side');
+		}
+		/*else if($(document).scrollTop()>$('#contact').position().top-300 && $('#header-contact').hasClass('current')==false)
+		{
+			resetHeaders()
+			$('#header-contact').addClass('current');
+			//console.log('contact')
+		}*/
+	});
 }
