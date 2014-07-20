b5ceaba6eb5bc982412aa0b9d264150b7f28b1d1
Sina Mahmoodi <itz.s1na@gmail.com>
2014-07-20 13:20:44 +0430

    Make intro static.

--- mirad-intro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from views import *
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-	(r'^$', home),)
